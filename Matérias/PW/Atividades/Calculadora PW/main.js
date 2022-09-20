e2d2adb03e362b4e76029d40d35d85ae769871e2
nickolss <nickolasmaiaraujo@gmail.com>
2022-09-19 21:22:20 -0300

    Adicionado calculadora de pw

---
+++ Matérias/PW/Atividades/Calculadora PW/main.js
@@ -0,0 +1,18 @@
+class Calculadora{
+    somar(a,b){
+        return a+b
+    }
+
+    subtrair(a,b){
+        return a-b
+    }
+
+    multiplicar(a,b){
+        a*b
+    }
+    
+    dividir(a,b){
+        a/b
+    }
+
+}
