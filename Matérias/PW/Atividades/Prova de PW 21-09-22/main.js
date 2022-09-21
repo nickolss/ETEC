257d30e4844b423ebde37a4fb147c5431776e508
nickolss <nickolasmaiaraujo@gmail.com>
2022-09-21 17:23:55 -0300

    Adicionei base da prova de PW do dia 21-09-22

---
+++ Matérias/PW/Atividades/Prova de PW 21-09-22/main.js
@@ -0,0 +1,34 @@
+class Calculadora{
+    somar(a,b){
+        return a+b
+    }
+
+    subtrair(a,b){
+        return a-b
+    }
+
+    multiplicar(a,b){
+        return a*b
+    }
+
+    dividir(a,b){
+        return a/b
+    }
+}
+
+
+function excluir(){
+    let input1 = document.getElementById('txtn1')
+    let input2 = document.getElementById('txtn2')
+
+    
+
+    /*
+    if(input1.value.length != 0 || input2.value.length != 0){
+        input1.innerHTML = ""
+        input2.innerHTML = ""
+    }else{
+        window.alert('Caixas de entrada já limpas!')
+    }
+    */
+}
