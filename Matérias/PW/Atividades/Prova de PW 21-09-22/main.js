46aa33db3ebf57e48be3963ab01cb041821ae24f
nickolss <nickolasmaiaraujo@gmail.com>
2022-09-21 20:55:23 -0300

    Terminado prova de PW

--- Matérias/PW/Atividades/Prova de PW 21-09-22/main.js
+++ Matérias/PW/Atividades/Prova de PW 21-09-22/main.js
@@ -1,34 +1,73 @@
-class Calculadora{
-    somar(a,b){
-        return a+b
-    }
+class Calculadora {
+	somar(a, b) {
+		return a + b;
+	}
 
-    subtrair(a,b){
-        return a-b
-    }
+	subtrair(a, b) {
+		return a - b;
+	}
 
-    multiplicar(a,b){
-        return a*b
-    }
+	multiplicar(a, b) {
+		return a * b;
+	}
 
-    dividir(a,b){
-        return a/b
-    }
+	dividir(a, b) {
+		return a / b;
+	}
 }
 
+//Variaveis
+let minhacalc = new Calculadora();
 
-function excluir(){
-    let input1 = document.getElementById('txtn1')
-    let input2 = document.getElementById('txtn2')
+//Functions das operações
+function somarn() {
+	let txtn1 = document.getElementById("txtn1");
+	let txtn2 = document.getElementById("txtn2");
+	let n1 = Number(txtn1.value);
+	let n2 = Number(txtn2.value);
+	let resp = document.getElementById("resp");
+	let conta = minhacalc.somar(n1, n2);
+	resp.innerHTML = `A soma entre ${n1} e ${n2} é igual a ${conta}`;
+}
 
-    
+function subtrairn() {
+	let txtn1 = document.getElementById("txtn1");
+	let txtn2 = document.getElementById("txtn2");
+	let n1 = Number(txtn1.value);
+	let n2 = Number(txtn2.value);
+	let resp = document.getElementById("resp");
+	let conta = minhacalc.subtrair(n1, n2);
+	resp.innerHTML = `A subtração entre ${n1} e ${n2} é igual a ${conta}`;
+}
 
-    /*
-    if(input1.value.length != 0 || input2.value.length != 0){
-        input1.innerHTML = ""
-        input2.innerHTML = ""
-    }else{
-        window.alert('Caixas de entrada já limpas!')
-    }
-    */
+function multiplicarn() {
+	let txtn1 = document.getElementById("txtn1");
+	let txtn2 = document.getElementById("txtn2");
+	let n1 = Number(txtn1.value);
+	let n2 = Number(txtn2.value);
+	let resp = document.getElementById("resp");
+	let conta = minhacalc.multiplicar(n1, n2);
+	resp.innerHTML = `A multiplicação entre ${n1} e ${n2} é igual a ${conta}`;
+}
+
+function dividirn() {
+	let txtn1 = document.getElementById("txtn1");
+	let txtn2 = document.getElementById("txtn2");
+	let n1 = Number(txtn1.value);
+	let n2 = Number(txtn2.value);
+	let resp = document.getElementById("resp");
+	let conta = minhacalc.dividir(n1, n2);
+	resp.innerHTML = `A divisão entre ${n1} e ${n2} é igual a ${conta}`;
+}
+
+//functions de funções
+
+function excluir() {
+	let txtn1 = document.getElementById("txtn1");
+	let txtn2 = document.getElementById("txtn2");
+	let resp = document.getElementById("resp");
+
+    txtn1.value = ""
+    txtn2.value = ""
+    resp.innerHTML = ""
 }
