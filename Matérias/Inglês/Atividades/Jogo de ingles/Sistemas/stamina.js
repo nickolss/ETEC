536c4f6be7ea6682022f447a36350a6d808f8885
nickolas_maia <99838582+nickolss@users.noreply.github.com>
2022-10-02 20:49:05 -0300

    Adicionado material da escola de empreendedores

--- Matérias/Inglês/Atividades/Jogo de ingles/Sistemas/stamina.js
+++ Matérias/Inglês/Atividades/Jogo de ingles/Sistemas/stamina.js
@@ -1,39 +1,41 @@
-class acoes{
-    perdersta(n1,n2){
-        return n1-n2
-    }
+class acoes {
+	perdersta(n1, n2) {
+		return n1 - n2;
+	}
 
-    ganharsta(n1,n2){
-        return n1+n2
-    }
+	ganharsta(n1, n2) {
+		return n1 + n2;
+	}
 }
 
 //variaveis
-var acao = new acoes()
-var stamina = [5,4,3,2,1,0]
-var resp = document.getElementById('resp')
+var acao = new acoes();
+// var stamina = [5,4,3,2,1,0]
+var resp = document.getElementById("resp");
 
+function atacar(n1) {
+	let stamina = [10];
 
-function atacar(n1){
-    
-    if(stamina[0]==5){
-        stamina = [4,3,2,1,0]
-        alert(stamina[0])
-    }else if(stamina[0]==4){
-        stamina = [3,2,1,0]
-        alert(stamina[0])
-    }else if(stamina[0]==3){
-        stamina = [2,1,0]
-        alert(stamina[0])
-    }else if(stamina[0] ==2){
-        stamina = [1,0]
-        alert(stamina[0])
-    }else if(stamina[0] ==1){
-        stamina = [0]
-        alert(stamina[0])
-    }else if(stamina[0] == 0){
-        alert('SEM STAMINA')
-    }
+	if (stamina != 0) {
+		if (stamina[0] == 5) {
+			stamina = [4, 3, 2, 1, 0];
+			alert(stamina[0]);
+		} else if (stamina[0] == 4) {
+			stamina = [3, 2, 1, 0];
+			alert(stamina[0]);
+		} else if (stamina[0] == 3) {
+			stamina = [2, 1, 0];
+			alert(stamina[0]);
+		} else if (stamina[0] == 2) {
+			stamina = [1, 0];
+			alert(stamina[0]);
+		} else if (stamina[0] == 1) {
+			stamina = [0];
+			alert(stamina[0]);
+		} else if (stamina[0] == 0) {
+			alert("SEM STAMINA");
+		}
 
-    alert(staminaat)
+		alert(staminaat);
+	}
 }
