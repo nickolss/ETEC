2d223f99da2f71aaefd53548996a220ee3608033
nickolas_maia <99838582+nickolss@users.noreply.github.com>
2023-02-09 22:51:38 -0300

    Adicionados 5 exercicios da aula do dia 09-02-2023

---
+++ 2° Ano/DS/Ex05/src/main/java/com/mycompany/ex05/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex05;
+
+/**
+ *
+ * @author nicks
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Util util = new Util();
+        util.receberValores();
+    }
+}
