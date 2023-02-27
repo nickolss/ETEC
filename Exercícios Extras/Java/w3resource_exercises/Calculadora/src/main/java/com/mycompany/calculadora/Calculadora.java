867df31564904fd7bf81bea9290752b8f5149450
nickolss <nickolasmaiaraujo@gmail.com>
2023-02-27 16:18:02 -0300

    Adicionada aula 27-02-23 de PW2

--- Exercícios Extras/Java/w3resource_exercises/Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
+++ Exercícios Extras/Java/w3resource_exercises/Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -1,59 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.calculadora;
-
-/**
- *
- * @author nickolas
- */
-public class Calculadora {
-    private int n1,n2;
-    private double resultado;
-    
-
-    public void setN1(int n1) {
-        this.n1 = n1;
-    }
-
-    public int getN1() {
-        return n1;
-    }
-
-    public void setN2(int n2) {
-        this.n2 = n2;
-    }
-
-    public int getN2() {
-        return n2;
-    }
-    
-    
-    public int soma(int n1, int n2){
-        return n1+n2;
-    }
-    
-    public int subtracao(int n1, int n2){
-        return n1-n2;
-    }
-    
-    public double multiplicacao(int n1, int n2){
-        return n1*n2;
-    }
-    
-    public double divisao(int n1, int n2){
-        return n1/n2;
-    }
-
-    public void setResultado(double resultado) {
-        this.resultado = resultado;
-    }
-    
-    
-
-    public double getResultado() {
-        System.out.print("O resultado é ");
-        return resultado;
-    }
-}
+                     "*-0x9874-*-0x1002-*-*-*-0x81",
+                            "*-0x9874-*-0x1002-*-*-*-0x84",
+                            "*-0x9874-*-0x1002-*-*-*-0x85",
+                            "*-0x9874-*-0x1002-*-*-*-0x87",
+                            "*-0x9874-*-0x1002-*-*-*-0x88",
+                            "*-0x9874-*-0x1002-*-*-*-0x89",
+                            "*-0x9874-*-0x1002-*-*-*-0xC4",
+                            "*-0x9874-*-0x1002-*-*-*-0xC5",
+                            "*-0x9874-*-0x1002-*-*-*-0xC6",
+                            "*-0x9874-*-0x1002-*-*-*-0xC7",
+                            "*-0x9874-*-0x1002-*-*-*-0xC8",
+                            "*-0x9874-*-0x1002-*-*-*-0xC9",
+                            "*-0x9874-*-0x1002-*-*-*-0xCA",
+                            "*-0x9874-*-0x1002-*-*-*-0xCB",
+                            "*-0x9874-*-0x1002-*-*-*-0xCC",
+                            "*-0x9874-*-0x1002-*-*-*-0xCD",
+                            "*-0x9874-*-0x1002-*-*-*-0xCE",
+                            "*-0x9874-*-0x1002-*-*-*-0xE1",
+                            "*-0x9874-*-0x1002-*-*-*-0xE2",
+        
