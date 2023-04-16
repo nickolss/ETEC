ed2b8a144d66b13fac25018bc33b2de04b374e21
nickolss <nickolasmaiaraujo@gmail.com>
2023-04-16 19:01:00 -0300

    Inicialize votacao page for pw class

---
+++ 2° Ano/PW 2/18-04-23/votacao/index.php
@@ -0,0 +1,58 @@
+<?php 
+    $votoFeijoada = 0;
+    $votoBaiao = 0;
+    $votoMoqueca = 0;
+
+    function confirmarVoto(){
+        echo "<p>Voto realizado com sucesso!</p>";
+    }
+
+    if(isset($_GET['id']) == 1){
+        $votoFeijoada++;
+    }else if(isset($_GET['id']) == 2){
+        $votoMoqueca++;
+    }else if(isset($_GET['id']) == 3){
+        $votoBaiao++;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Votação</title>
+    <link rel="stylesheet" href="assets/styles/reset.css">
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <script src="exibirTexto.js"></script>
+</head>
+<body>
+    <main class="main">
+    <h1 class="main__titulo">Melhores Pratos Brasileiros</h1>
+        <div class="cards">
+            <div class="card">
+                <img src="assets/img/feijoada.png" alt="Feijoada" class="card__imagem">
+                <h1 class="card__titulo">Feijoada</h1>
+                <p class="card__descricao">Feijoada é um prato brasileiro que consiste em um ensopado de feijão preto com carnes variadas, acompanhado por arroz, farofa, couve e laranja. É uma refeição saborosa e completa, bastante popular no Brasil e em outros países.</p>
+                <a href="index.php?id=1" class="card__link">Vote</a>
+            </div>
+            <div class="card card__moqueca">
+                <img src="assets/img/moqueca.jpg" alt="Moqueca" class="card__imagem">
+                <h1 class="card__titulo">Moqueca</h1>
+                <p class="card__descricao">Moqueca é um prato típico da culinária brasileira, originário do estado da Bahia, que consiste em um ensopado de peixe ou frutos do mar cozido com tomates, cebola, pimentão, leite de coco e dendê. É servido com arroz e pirão, uma mistura de farinha e caldo do cozimento.</p>
+            <a href="index.php?id=2" class="card__link" >Vote</a>
+            </div>
+            <div class="card card__baiao">
+                <img src="assets/img/baiao de dois.jpg" alt="Baião de Dois" class="card__imagem">
+                <h1 class="card__titulo">Baião de Dois</h1>
+                <p class="card__descricao">Baião de dois é um prato típico da culinária nordestina brasileira, que consiste em um refogado de arroz e feijão-de-corda, cozido com diversos ingredientes, como carne seca, linguiça, bacon, cebola, alho, coentro e pimenta. É uma refeição completa e muito saborosa, geralmente acompanhada por farofa, vinagrete e salada.</p>
+                <a href="index.php?id=3" class="card__link" >Vote</a>
+            </div>
+        </div>
+        <div class="enviar">
+            <a href="" class="enviar__botao">Terminar Votação</a>
+        </div>
+    </main>
+</body>
+</html>
