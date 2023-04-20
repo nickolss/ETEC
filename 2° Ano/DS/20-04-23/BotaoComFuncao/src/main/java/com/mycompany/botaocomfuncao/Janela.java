bedfbb861d1b62386d60d5a80062a99d7bf79543
nickolss <nickolasmaiaraujo@gmail.com>
2023-04-20 15:01:43 -0300

    Added activy botaoComEvento

---
+++ 2° Ano/DS/20-04-23/BotaoComFuncao/src/main/java/com/mycompany/botaocomfuncao/Janela.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.botaocomfuncao;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ *
+ * @author dti
+ */
+public class Janela extends JFrame{
+    JButton botaoSair;
+    Janela(){
+        super("Exemplo botão com função");
+        Container tela = getContentPane();
+        setLayout(null);
+        setSize(300, 150);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        
+        botaoSair = new JButton("Sair");
+        botaoSair.setBounds(100,50,100,20);
+        botaoSair.addActionListener(
+                new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        System.exit(0);
+                    }
+                }
+        );
+        
+        tela.add(botaoSair);
+        
+    }
+}
