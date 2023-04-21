16bf3ddd3343f890120b12c8d4a288529ce2d384
nickolss <nickolasmaiaraujo@gmail.com>
2023-04-21 12:25:51 -0300

    Added Calculadora project

---
+++ Exercícios Extras/Java/Exercícios Soltos/Calculadora/src/main/java/com/mycompany/calculadora/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+import javax.swing.*;
+/**
+ *
+ * @author nicks
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
