40eb3afb1e5950c738042cfa0f9fd2a003d591d6
nickolss <nickolasmaiaraujo@gmail.com>
2023-05-04 21:46:36 -0300

    Finalize cadastro cliente project

--- Exercícios Extras/Java/Exercícios Soltos/CadastroCliente/src/main/java/com/mycompany/cadastrocliente/Janela.java
+++ Exercícios Extras/Java/Exercícios Soltos/CadastroCliente/src/main/java/com/mycompany/cadastrocliente/Janela.java
@@ -5,17 +5,19 @@
 package com.mycompany.cadastrocliente;
 import java.awt.*;
 import java.awt.event.*;
+import java.text.ParseException;
 import javax.swing.*;
+import javax.swing.text.*;
 /**
  *
  * @author dti
  */
 public class Janela extends JFrame{
     JLabel titulo, rotuloNome, rotuloCPF, rotuloRG, rotuloEndereco, rotuloBairro, rotuloCEP, rotuloCidade, rotuloEstado, rotuloLogin, rotuloSenha, rotuloTelefone, rotuloCelular, rotuloEmail;
-    
-    JTextField campoNome, campoCPF, campoRG, campoEndereco, campoBairro, campoCEP, campoCidade, campoEstado, campoLogin, campoTelefone, campoCelular, campoEmail;;
-    
+    JFormattedTextField campoCPF, campoRG, campoCEP, campoEstado, campoTelefone, campoCelular;
+    JTextField campoNome,campoEndereco, campoBairro, campoCidade, campoLogin, campoEmail;
     JPasswordField campoSenha;
+    MaskFormatter mascaraCPF, mascaraRG, mascaraCEP, mascaraEstado, mascaraTelefone, mascaraCelular;
             
     Janela(){
         super("Cadastro de Clientes");
@@ -41,17 +43,11 @@ public class Janela extends JFrame{
         rotuloEmail = new JLabel("Email: ");
         
         campoNome = new JTextField(20);
-        campoCPF = new JTextField(12);
-        campoRG = new JTextField(12);
         campoEndereco = new JTextField(20);
         campoBairro = new JTextField(15);
-        campoCEP = new JTextField(5);
         campoCidade = new JTextField(16); 
-        campoEstado = new JTextField(4);
         campoLogin = new JTextField(14);
         campoSenha = new JPasswordField(14);
-        campoTelefone = new JTextField(16);
-        campoCelular = new JTextField(4);
         campoEmail = new JTextField(20);
         
         
@@ -65,16 +61,54 @@ public class Janela extends JFrame{
         rotuloBairro.setBounds(10, 170, 50, 20);
         rotuloCEP.setBounds(320, 170, 50, 20);
         rotuloCidade.setBounds(10, 210, 50, 20);
-        rotuloEstado.setBounds(380, 210, 50, 20);
+        rotuloEstado.setBounds(360, 210, 50, 20);
         rotuloLogin.setBounds(10, 250, 50, 20);
         rotuloSenha.setBounds(10, 290, 50, 20);
         rotuloTelefone.setBounds(10, 330, 80, 20);
         rotuloCelular.setBounds(310, 330, 80, 20);
         rotuloEmail.setBounds(10, 370, 50, 20);
         
-        campoNome.setBounds(50,50,400,20);
-        campoCPF.setBounds(50, 90, 250, 20);
-        campoEmail.setBounds(50, 370, 400, 20);
+        
+        try{
+            mascaraCPF = new MaskFormatter("###.###.###-##");
+            mascaraRG = new MaskFormatter("##.###.###-A");
+            mascaraCEP = new MaskFormatter("#####-###");
+            mascaraEstado = new MaskFormatter("??");
+            mascaraTelefone = new MaskFormatter("(##)#####-####");
+            mascaraCelular = new MaskFormatter("(##)#####-####");
+            
+            mascaraCPF.setPlaceholder("_");
+            mascaraRG.setPlaceholder("_");
+            mascaraCEP.setPlaceholder("_");
+            mascaraEstado.setPlaceholder("_");
+            mascaraTelefone.setPlaceholder("_");
+            mascaraCelular.setPlaceholder("_");
+        }catch(ParseException e){
+            
+        }
+        
+        campoCPF = new JFormattedTextField(mascaraCPF);
+        campoRG = new JFormattedTextField(mascaraRG);
+        campoCEP = new JFormattedTextField(mascaraCEP);
+        campoEstado = new JFormattedTextField(mascaraEstado);
+        campoTelefone = new JFormattedTextField(mascaraTelefone);
+        campoCelular = new JFormattedTextField(mascaraCelular);
+        
+        campoNome.setBounds(70,50,380,20);
+        campoCPF.setBounds(70, 90, 220, 20);
+        campoRG.setBounds(325, 90, 125, 20);
+        campoEndereco.setBounds(70, 130, 380, 20);
+        campoBairro.setBounds(70, 170, 240, 20);
+        campoCEP.setBounds(360, 170, 90, 20);
+        campoCidade.setBounds(70, 210, 280, 20);
+        campoEstado.setBounds(405, 210, 45, 20);
+        campoLogin.setBounds(70, 250, 220, 20);
+        campoSenha.setBounds(70, 290, 220, 20);
+        campoTelefone.setBounds(70, 330, 235, 20);
+        campoCelular.setBounds(370, 330, 80, 20);
+        campoEmail.setBounds(70, 370, 380, 20);
+        
+        
         
         setVisible(true);
         tela.add(titulo);
@@ -94,6 +128,16 @@ public class Janela extends JFrame{
         
         tela.add(campoNome);
         tela.add(campoCPF);
+        tela.add(campoRG);
+        tela.add(campoEndereco);
+        tela.add(campoBairro);
+        tela.add(campoCEP);
+        tela.add(campoCidade);
+        tela.add(campoEstado);
+        tela.add(campoLogin);
+        tela.add(campoSenha);
+        tela.add(campoTelefone);
+        tela.add(campoCelular);
         tela.add(campoEmail);
     }
 }
