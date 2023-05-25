af4bf1a8dd7f7fbd15f924c2d1351896d0bf3b87
nickolss <nickolasmaiaraujo@gmail.com>
2023-05-24 22:33:37 -0300

    Finalize AppConversor from PAM class

--- 2° Ano/PAM/24-05-23/appConversor/app/src/main/java/com/example/appconversor/MainActivity.kt
+++ 2° Ano/PAM/24-05-23/appConversor/app/src/main/java/com/example/appconversor/MainActivity.kt
@@ -2,39 +2,42 @@ package com.example.appconversor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var editText: EditText
-    lateinit var celsiusRadioButton: RadioButton
+    lateinit var celciusRadioButton: RadioButton
     lateinit var fahreinheitRadioButton: RadioButton
-    lateinit var convertButton: Button
+    lateinit var converterButton: Button
+    lateinit var resultado: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         editText = findViewById(R.id.valorTemp) as EditText
+        celciusRadioButton = findViewById(R.id.celcius) as RadioButton
+        fahreinheitRadioButton = findViewById(R.id.fahreinheit) as RadioButton
+        converterButton = findViewById(R.id.converterButton) as Button
+        resultado = findViewById(R.id.resultado) as TextView
 
-        celsiusRadioButton = findViewById(R.id.celsiusRadio) as RadioButton
-        fahreinheitRadioButton = findViewById(R.id.fahreinheitRadio) as RadioButton
-
-        convertButton = findViewById(R.id.converterButton) as Button
-        convertButton.setOnClickListener{converter()}
+        converterButton.setOnClickListener{converter()}
     }
 
     fun converter(){
-        var temp : Double = editText.toString().toDouble()
+        var temp: Double = editText.text.toString().toDouble()
 
-        if(celsiusRadioButton.isChecked){
-            temp = (temp - 32)* 5/9
+        if(celciusRadioButton.isChecked){
+            temp = (temp-32) * 5/9
         }else if(fahreinheitRadioButton.isChecked){
             temp = temp*9/5+32
         }
+
         editText.setText(temp.toString())
+        resultado.setText(temp.toString())
     }
 
 }
