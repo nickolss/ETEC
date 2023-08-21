74b406b89d9a7061ce81f64321d0e91c93a717e0
nickolas_maia <99838582+nickolss@users.noreply.github.com>
2023-08-21 16:34:17 -0300

    Adicionado aula 21-08-23 de PW

---
+++ 2° Ano/PW 2/21-08-23/resposta.php
@@ -0,0 +1,9 @@
+<?php 
+    $nome = $_POST['nome'];
+    $telefone = $_POST['telefone'];
+    $orige = $_POST['origem'];
+    $observacao = $_POST['observacao'];
+    $data = $_POST['dataContato'];
+
+    
+?>
