38c98b8546ead380739c6a8280be66a3371c2ce5
nickolss <nickolasmaiaraujo@gmail.com>
2024-05-17 23:21:33 -0300

    Adicionada tarefa da aula de pam 17-05-24

---
+++ 3° Ano/pam2/aula-17-05-24/app/src/main/java/com/example/aula_17_05_24/MainActivity.kt
@@ -0,0 +1,132 @@
+package com.example.aula_17_05_24
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import com.example.aula_17_05_24.ui.theme.Aula170524Theme
+import com.example.aula_17_05_24.ui.theme.B_Button
+import com.example.aula_17_05_24.ui.theme.I_Button
+import com.example.aula_17_05_24.ui.theme.Mb_Button
+import com.example.aula_17_05_24.ui.theme.R_Button
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Aula170524Theme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun NoteButton(
+    text: String,
+    buttonColors: ButtonColors = ButtonDefaults.buttonColors(),
+    block: () -> Unit
+){
+    ElevatedButton(
+        onClick = block,
+        shape = RoundedCornerShape(5.dp),
+        colors = buttonColors,
+        modifier = Modifier.width(150.dp)
+    ) {
+        Text(text = text)
+    }
+}
+
+@Composable
+fun App(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                Modifier.padding(vertical = 2.dp)
+            ) {
+                Image(painter = painterResource(id = R.drawable.logo_etec), contentDescription = "Logo da Etec")
+            }
+
+            Row(
+                Modifier.padding(vertical = 10.dp)
+            ){
+                Text(
+                    text = "ETEC DA ZONA LESTE",
+                    fontSize = 5.em
+                )
+            }
+
+            Column(
+                Modifier
+                    .fillMaxHeight()
+            ) {
+                NoteButton(
+                    text = "MB",
+                    buttonColors = Mb_Button(),
+                ) {
+                    Log.i(TAG, "Clicou em MB")
+                }
+                Spacer(modifier = Modifier.size(15.dp))
+                NoteButton(
+                    text = "B",
+                    buttonColors = B_Button(),
+                ) {
+                    Log.d(TAG, "Clicou em B")
+                }
+                Spacer(modifier = Modifier.size(15.dp))
+                NoteButton(
+                    text = "R",
+                    buttonColors = R_Button(),
+                ) {
+                    Log.w(TAG, "Clicou em R")
+                }
+                Spacer(modifier = Modifier.size(15.dp))
+                NoteButton(
+                    text = "I",
+                    buttonColors = I_Button(),
+                ) {
+                    Log.e(TAG, "Clicou em I")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppPreview(){
+    Aula170524Theme {
+        App()
+    }
+}
---
+++ 3° Ano/pam2/aula-17-05-24/app/src/main/java/com/example/aula_17_05_24/ui/theme/Theme.kt
@@ -0,0 +1,71 @@
+package com.example.aula_17_05_24.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+        primary = Purple80,
+        secondary = PurpleGrey80,
+        tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+        primary = Purple40,
+        secondary = PurpleGrey40,
+        tertiary = Pink40
+
+        /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun Aula170524Theme(
+        darkTheme: Boolean = isSystemInDarkTheme(),
+        // Dynamic color is available on Android 12+
+        dynamicColor: Boolean = true,
+        content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+    )
+}
+
+@Composable
+fun Mb_Button() = ButtonDefaults.buttonColors(containerColor = DarkGreen)
+
+@Composable
+fun B_Button() = ButtonDefaults.buttonColors(containerColor = DarkBlue)
+
+@Composable
+fun R_Button() = ButtonDefaults.buttonColors(containerColor = DarkOrange)
+
+@Composable
+fun I_Button() = ButtonDefaults.buttonColors(containerColor = DarkRed)
